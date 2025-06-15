52dda98fbbe7f4cc9d857ed0ab1e4e660a5e916f
silvachunk <silva@mirainu.com>
2025-06-14 19:08:10 -0700

    Update Kernel.php

--- app/Presenter/Http/Kernel.php
+++ app/Presenter/Http/Kernel.php
@@ -8,13 +8,6 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * These middleware are run during every request to your application.
-     *
-     * @var array<int, class-string|string>
-     */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
         \Illuminate\Http\Middleware\HandleCors::class,
@@ -23,11 +16,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
-    /**
-     * The application's route middleware groups.
-     *
-     * @var array<string, array<int, class-string|string>>
-     */
     protected $middlewareGroups = [
         'web' => [
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
@@ -43,13 +31,6 @@ class Kernel extends HttpKernel
         ],
     ];
 
-    /**
-     * The application's middleware aliases.
-     *
-     * Aliases may be used to conveniently assign middleware to routes and groups.
-     *
-     * @var array<string, class-string|string>
-     */
     protected $middlewareAliases = [
         'auth' => \App\Presenter\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
@@ -60,4 +41,17 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected function mapRoutes(): void
+    {
+        $this->loadRoutesFrom(base_path('app/Presenter/Http/Routes/web.php'));
+        $this->loadRoutesFrom(base_path('app/Presenter/Http/Routes/api.php'));
+    }
+
+    public function bootstrap(): void
+    {
+        parent::bootstrap();
+
+        $this->mapRoutes();
+    }
 }
