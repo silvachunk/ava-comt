e53974794bf6b2b64c9f982327ed3eb1d4bdc072
silvachunk <silva@mirainu.com>
2025-06-14 18:56:09 -0700

    Update api.php

--- app/Presenter/Http/Routes/api.php
+++ app/Presenter/Http/Routes/api.php
@@ -5,6 +5,12 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
+// Basic homepage or health check route
+Route::get('/', function () {
+    return 'Fanuda Ava is live!';
+});
+
+// Automatically load all v1 API routes
 foreach (File::allFiles(__DIR__ . '/v1') as $partial) {
     Route::prefix('/v1')->group($partial->getPathname());
 }
